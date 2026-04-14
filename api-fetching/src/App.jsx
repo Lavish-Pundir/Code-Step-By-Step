5f1e3dda018781720f2a43d3995cefa5961aad3b
Lavish-Pundir <your-new-lavishpundir90@gmail.com>
2026-04-14 15:37:17 +0530

    post method add data frontend to backend

--- api-fetching/src/App.jsx
+++ api-fetching/src/App.jsx
@@ -3,18 +3,66 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([])
+  const [todo, setTodo] = useState('')
+
+  // GET API (Fetch Data)
 
   useEffect(() => {
     fetch('https://dummyjson.com/todos')
       .then((result) => result.json())
       .then((resp) => {
-        setData(resp.todos)  
+        setData(resp.todos)
       })
   }, [])
 
+  // POST API (Add Data)
+
+  const addTodo = async () => {
+    if (!todo) return;
+
+    const response = await fetch('https://dummyjson.com/todos/add', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        todo: todo,
+        completed: false,
+        userId: 1
+      })
+    });
+
+    const result = await response.json();
+
+    setData([result, ...data])
+    setTodo('')
+  }
+
   return (
     <>
-      <h1>API Fetching</h1>
+      <h1>API Fetching + POST</h1>
+
+      {/* <input 
+      type="text"
+      value={todo}
+      onChange={(e) => setTodo(e.target.value)}
+      placeholder='Enter Todo'
+      />
+      <button onClick={addTodo}>Add Todo</button> */}
+
+      <div style={{ marginBottom: "20px" }}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Enter todo"      //  add post frontend code
+          style={{ padding: "8px", marginRight: "10px" }}
+        />
+
+        <button onClick={addTodo} style={{ padding: "8px 15px" }}>
+          Add Todo
+        </button>
+      </div>
 
       <table border="1">
         <thead>
