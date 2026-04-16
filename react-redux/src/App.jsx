24b44a85c2b6585104aec439f673d83895102d36
Lavish-Pundir <your-new-lavishpundir90@gmail.com>
2026-04-16 10:56:37 +0530

    create home component add img

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,14 +1,26 @@
-import React, { useState } from 'react'
-import User from './user'
+import { useState } from 'react'
 import './App.css'
+import HomeContainer from './containers/HomeContainer'
 
+{/* import User from './user'
 function App() {
   return (
     <div className='App'>
-      {/* <h1>React Readux</h1> */}
+    //    <h1>React Readux</h1> 
       <User data={{ name: "Lavish", age: 23 }} />
     </div>
   )
+}
+export default App */}
+
+
+function App() {
+    return (
+        <div>
+            <HomeContainer />
+            
+        </div>
+    )
 }
 
 export default App
